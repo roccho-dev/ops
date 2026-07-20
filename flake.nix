296c9a56402b0b5cb61c8065b3d3169ef8562db8
roccho <40359643+roccho-dev@users.noreply.github.com>
2026-07-20 22:30:07 +0900

    Declare flake inputs explicitly

--- flake.nix
+++ flake.nix
@@ -1,7 +1,29 @@
 {
   description = "ops: operational packages implementing governance contracts, including gosh v0";
 
-  inputs = (import ./flake.base.nix).inputs;
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    governance = {
+      url = "github:roccho-dev/governance/proposals";
+      flake = false;
+    };
+    adrsRecords = {
+      url = "path:./fixtures/adrsRecords";
+      flake = false;
+    };
+    conventionGovernance = {
+      url = "github:roccho-dev/governance/proposals";
+      inputs.adrsRecords.follows = "adrsRecords";
+    };
+    ops-build-defs = {
+      url = "path:./packages/ops-build-defs";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nodejs-src = {
+      url = "git+https://github.com/nodejs/node?ref=refs/tags/v26.3.0&rev=b7e6a5d37e7a14ef0f2cc95214b95d66c4081415";
+      flake = false;
+    };
+  };
 
   outputs =
     inputs@{ nixpkgs, ... }:
