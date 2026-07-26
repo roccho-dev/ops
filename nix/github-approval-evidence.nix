ea436b4905fba5b732572eaaf108ed5689b5a122
roccho <40359643+roccho-dev@users.noreply.github.com>
2026-07-26 19:25:10 +0900

    Add bounded Nix package for GitHub approval evidence

---
+++ nix/github-approval-evidence.nix
@@ -0,0 +1,9 @@
+{ pkgs ? import <nixpkgs> {} }:
+
+pkgs.writeShellApplication {
+  name = "github-approval-evidence";
+  runtimeInputs = [ pkgs.python3 ];
+  text = ''
+    exec ${pkgs.python3}/bin/python3 ${../tools/github-approval-evidence.py} "$@"
+  '';
+}
